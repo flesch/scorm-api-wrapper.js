7eb9562e1e709173cedf938fb3f5d1c3272c91e4
John Flesch <john@fles.ch>
2012-01-25 13:57:43 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,33 @@
+# api-wrapper.js
+
+## About
+
+While SCORM is a standard in need of [some updating](http://scorm.com/tincan/), some of us still have to use it. The wrapper created by ADL was really only meant as a demo and doesn't include many of the features we think should be there.
+
+  - LMS interaction is batched, instead of real-time. `LMSSetValue` will cache any data you want to send to the LMS. `LMSCommit` loops through the cache and sends the data. _(Why send `cmi.core.lesson_location` every time it changes, we the LMS really only cares about the last value?)_
+  - If a learner's status is `not attempted`, automatically set her to `incomplete`.
+  - Using an interval, check that the LMS connection is still active. If it is, call `LMSCommit`, if not, throw `onLMSConnectionError` (which you can define and kick the user out of the course).
+  - Automatically handle setting `cmi.core.session_time`.
+  - Refresh the LMS's "Course Status" page on `LMSFinish` (PeopleSoft ELM only).
+
+At Verizon Wireless, we have a PeopleSoft based LMS and as with any enterprise software, there is just some general weirdness about it. Our wrapper attempts to deal with that. _(As of now, much of this is uncommented, but it shouldn't throw any errors in other LMS implementations.)_
+
+## Usage
+
+`api-wrapper.js` is intended to be a drop in replacement for ADL's wrapper. As a developer, you won't need to change any of your code. Simply overwrite your old API wrapper with this one.
+
+## Dependencies
+
+`api-wrapper.js` assumes the browser you're using supports *JSON*. We use [json2.js](https://github.com/douglascrockford/JSON-js) in our courses, or you can easily remove that dependency.
+
+We didn't want to have to serialize objects before _setting_ them:
+
+    LMSSetValue("cmi.suspend_data", {"key":"value"});
+
+## Limitations
+
+This file attempts to support both `LMSInitialize` and `doLMSInitialize`, so the global namespace is very polluted.
+
+## License
+
+Released under the MIT License. Enjoy!
---
+++ api-wrapper.js
@@ -0,0 +1,219 @@
+!(function () {
+
+  var api, container, frame, session, cache = [], store = [], breaker = {}, prefix, methods, keepalive,
+  // Save bytes in the minified version.
+  array_proto = Array.prototype,
+  native_forEach = array_proto.forEach,
+  native_every = array_proto.every;
+
+  // Capture the LMS API. We only need to search for it once, so we can cache it in a variable and re-use it.
+  // Some SCORM API Wrappers will search for the API with every SCORM command. Is it really going to move?
+  api = (function (search) {
+    // We've made the `container` variable accessible outside of this function, as we'll
+    // want to interact with the DOMWindow that holds the API.
+    var self = arguments.callee;
+    container = search || window;
+    // If the API isn't in the current window, look backwards for it.
+    if (!container.API) {
+      // First, we'll try any parent frames.
+      if (container !== container.parent) {
+        return self.call(this, container.parent);
+      }
+      // Then, any `opener` windows.
+      if (container.opener !== null && (typeof container.opener !== "undefined") && !container.opener.closed) {
+        return self.call(this, container.opener);
+      }
+    }
+    if (container.parent.frames && "SCODataFrame" in container.parent.frames) {
+      frame = container.parent.frames.SCODataFrame;
+    }
+    // Return the API Object.
+    return container.API;
+  }).call(this);
+  
+  // https://github.com/documentcloud/underscore/blob/master/underscore.js#L75
+  function each(obj, iterator, context) {
+    if (obj === null) { return; }
+    if (native_forEach && obj.forEach === native_forEach) {
+      obj.forEach(iterator, context);
+    } else if (obj.length === +obj.length) {
+      for (var i = 0, l = obj.length; i < l; i++) {
+        if (i in obj && iterator.call(context, obj[i], i, obj) === breaker) {
+          return;
+        }
+      }
+    } else {
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          if (iterator.call(context, obj[key], key, obj) === breaker) {
+            return;
+          }
+        }
+      }
+    }
+  }
+
+  // https://github.com/documentcloud/underscore/blob/master/underscore.js#L175
+  function every(obj, iterator, context) {
+    var result = true;
+    if (obj === null) { return result; }
+    if (native_every && obj.every === native_every) {
+      return obj.every(iterator, context);
+    }
+    each(obj, function(value, index, list) {
+      if (!(result = result && iterator.call(context, value, index, list))) {
+        return breaker;
+      }
+    });
+    return result;
+  }
+
+  function contains(a, obj) {
+    var i = a.length;
+    while (i--) {
+      if (a[i] === obj) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Returns a HHHH:MM:SS
+  function format_duration(start, end) {
+    var duration = end - start, h, m, s;
+    h = /[0-9]{4}$/.exec("000" + Math.floor(duration / 3600000));
+    m = Math.floor((duration % 3600000) / 60000);
+    s = Math.floor(((duration % 3600000) % 60000) / 1000);
+    return [h, (m < 10 ? "0" + m : m), (s < 10 ? "0" + s : s)].join(":");
+  }
+
+  function LMSGetValue(model) {
+    var value;
+    if (cache[model]) { return cache[model]; }
+    if (api) {  
+      if ((value = api.LMSGetValue(model)) && +api.LMSGetLastError() === 0) {
+        cache[model] = value;
+        return cache[model];
+      }
+    }
+    return null;
+  }
+
+  function LMSSetValue(model, value) {
+    cache[model] = (typeof value !== "string") ? JSON.stringify(value) : value;
+    if (!contains(store, model)) { store.push(model); }
+    return cache[model];
+  }
+
+  function LMSGetLastError() {
+    return Number(api.LMSGetLastError()) || null;
+  }
+
+  function LMSGetErrorString(errorCode) {
+    return api.LMSGetErrorString(String(errorCode)) || null;
+  }
+
+  function LMSGetDiagnostic(errorCode) {
+    return api.LMSGetDiagnostic(String(errorCode)) || null;
+  }
+
+  function LMSCommit() {
+    var commit = false;
+    if (api) {
+      commit = every(store, function(model, index, list){
+        if (api.LMSSetValue(model, cache[model]) === "true" && +api.LMSGetLastError() === 0) {
+          return true;
+        }
+        return false;
+      });
+      if (commit && (store = []) && api.LMSCommit("") === "true" && +api.LMSGetLastError() === 0) {
+        return commit;
+      }
+    }
+    return commit;
+  }
+
+  function LMSInitialize() {
+    if (api && api.LMSInitialize("") === "true" && /0|101/.test(+api.LMSGetLastError())) {
+
+      // Capture when we've started communication. We'll use this timestamp to determine
+      // how long a learner has been in the course.
+      session = +new Date;
+
+      // As a convenience to the developer, we'll mark the learner incomplete when the first session starts.
+      if (!/(pass|complet)ed?/.test(LMSGetValue("cmi.core.lesson_status"))) {
+        LMSSetValue("cmi.core.lesson_status", "incomplete");
+      }
+
+      // In PeopleSoft ELM, `LMSCommit` triggers an alert, which can be pretty annoying.
+      // This will remove that (and we'll fake it in LMSFinish).
+      if (frame && container.mode === "test" && "displaySuccess" in frame) {
+        frame._displaySuccess = frame.displaySuccess;
+        frame.displaySuccess = function(){};
+      }
+
+      if (!keepalive) {
+        keepalive = window.setInterval(function(){
+          if (!LMSCommit()) {
+            window.clearInterval(keepalive);
+            if (window.onLMSConnectionError) {
+              window.onLMSConnectionError();
+            }
+          }
+        }, 300000); // 5 minutes
+      }
+
+      return true;
+    }
+    return false;
+  }
+
+  function LMSFinish() {  
+    if (api) {
+      // Let's track how long the user's session was.
+      LMSSetValue("cmi.core.session_time", format_duration(session, +new Date));
+      // LMSCommit will send anything in the cache to the LMS.
+      if (LMSCommit() && (api.LMSFinish("") === "true")) {
+        if (frame) {
+          if (container.mode === "test" && "_displaySuccess" in frame) {
+            frame._displaySuccess.call(container);
+          }
+          // This throws an error once in a while. Let's hide it.
+          frame.cleanup = function(){};
+          // Everybody goes crazy for this... This just refreshes the page, so that it
+          // appears as if the status is updated in realtime.
+          container.location.href = container.location.href;
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Change  
+  function to_string(fn) {
+    return function(){
+      return fn.apply(window, arguments).toString();  
+    }
+  }
+
+  // Yay, now let's pollute the global object with all these SCORM methods.
+  window.getAPI = window.findAPI = window.getAPIHandle = function () { return api; };
+  window.LMSInitialize = LMSInitialize;
+  window.LMSCommit = LMSCommit;
+  window.LMSFinish = LMSFinish;
+  window.LMSGetValue = LMSGetValue;
+  window.LMSSetValue = LMSSetValue;
+  window.LMSGetLastError = LMSGetLastError;
+  window.LMSGetErrorString = LMSGetErrorString;
+  window.LMSGetDiagnostic = LMSGetDiagnostic;
+  window.doLMSInitialize = to_string(LMSInitialize);
+  window.doLMSCommit = to_string(LMSCommit);
+  window.doLMSFinish = to_string(LMSFinish);
+  window.doLMSGetValue = to_string(LMSGetValue);
+  window.doLMSSetValue = to_string(LMSSetValue);
+  window.doLMSGetLastError = to_string(LMSGetLastError);
+  window.doLMSGetErrorString = to_string(LMSGetErrorString);
+  window.doLMSGetDiagnostic = to_string(LMSGetDiagnostic);
+
+}).call(this);
